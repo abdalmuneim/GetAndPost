60ba4182d339cf4edbbe99802f2e404846272265
Abdalmuneim <mo.m.eg21@gmail.com>
2021-10-25 23:29:36 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -45,43 +45,40 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: FutureBuilder(
-          future: brand,
-          builder:
-              (BuildContext context, AsyncSnapshot<List<Brand>?> snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-                return const Center(child: Text('none'));
-              case ConnectionState.waiting:
-                return const Center(child: CircularProgressIndicator());
-              case ConnectionState.active:
-                return const Center(child: Text(''));
-              case ConnectionState.done:
-                if (snapshot.hasError) {
-                  return Center(
-                      child: Text('${snapshot.error}',
-                          style: const TextStyle(
-                              color: Colors.red, fontSize: 20)));
-                } else {
-                  return Padding(
-                    padding: const EdgeInsets.only(top: 10),
-                    child: ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Center(
-                          child: ListTile(
-                            title: Text('${snapshot.data![index].id}'),
-                            subtitle: Text(snapshot.data![index].title),
-                          ),
-                        );
-                      },
-                    ),
-                  );
-                }
-            }
-          },
-        ),
+      body: FutureBuilder(
+        future: brand,
+        builder:
+            (BuildContext context, AsyncSnapshot<List<Brand>?> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return const Center(child: Text('none'));
+            case ConnectionState.waiting:
+              return const Center(child: CircularProgressIndicator());
+            case ConnectionState.active:
+              return const Center(child: Text(''));
+            case ConnectionState.done:
+              if (snapshot.hasError) {
+                return Center(
+                    child: Text('${snapshot.error}',
+                        style: const TextStyle(
+                            color: Colors.red, fontSize: 20)));
+              } else {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        title: Text('${snapshot.data![index].id}'),
+                        subtitle: Text(snapshot.data![index].title),
+
+                      );
+                    },
+                  ),
+                );
+              }
+          }
+        },
       ),
     );
   }
