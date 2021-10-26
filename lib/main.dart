603d667fe20eef8fb190fb5b9cf87fc456406d66
@abdelmunein <mo.m.eg21@gmail.com>
2021-10-26 10:30:25 +0200

    suneUpData

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late Future<List<Album>?> brand;
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -39,46 +40,96 @@ class _MyHomePageState extends State<MyHomePage> {
     brand = GetDat().fetchData();
   }
 
+  // const Duration(milliseconds: 3000),
+  // () => setState(() {
+  // isLoading = false;
+  // });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: FutureBuilder(
-        future: brand,
-        builder:
-            (BuildContext context, AsyncSnapshot<List<Album>?> snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              return const Center(child: Text('none'));
-            case ConnectionState.waiting:
-              return const Center(child: CircularProgressIndicator());
-            case ConnectionState.active:
-              return const Center(child: Text(''));
-            case ConnectionState.done:
-              if (snapshot.hasError) {
-                return Center(
-                    child: Text('${snapshot.error}',
-                        style: const TextStyle(
-                            color: Colors.red, fontSize: 20)));
-              } else {
-                return Center(
-                  child: ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        title: Text('${snapshot.data![index].id}'),
-                        subtitle: Text(snapshot.data![index].title),
-
-                      );
+      body: Center(
+        child: Column(
+          children: [
+            OutlinedButton(
+              onPressed: () => buildFutureBuilder(),
+              child: const Text('GET DATA',
+                  style: TextStyle(color: Colors.white, fontSize: 25)),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith(
+                      (states) => Colors.green)),
+            ),
+            const SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: isLoading
+                  ? null
+                  : () {
+                      print(isLoading);
+                      Future.delayed(
+                          const Duration(milliseconds: 3000),
+                          () => {
+                                setState(() {
+                                  isLoading = false;
+                                }),
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text('Post Successful'))),
+                                print(isLoading),
+                              });
+                      setState(() {
+                        isLoading = true;
+                      });
+                      print(isLoading);
                     },
-                  ),
-                );
-              }
-          }
-        },
+              child: isLoading
+                  ? const Text('Processing your request, please wait...',
+                      style: TextStyle(color: Colors.amber, fontSize: 25))
+                  : const Text('POST DATA',
+                      style: TextStyle(color: Colors.amber, fontSize: 25)),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith(
+                      (states) => isLoading ? Colors.red : Colors.blueAccent)),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  buildFutureBuilder() {
+    return FutureBuilder(
+      future: brand,
+      builder: (BuildContext context, AsyncSnapshot<List<Album>?> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return const Center(child: Text('none'));
+          case ConnectionState.waiting:
+            return const Center(child: CircularProgressIndicator());
+          case ConnectionState.active:
+            return const Center(child: Text(''));
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return Center(
+                  child: Text('${snapshot.error}',
+                      style: const TextStyle(color: Colors.red, fontSize: 20)));
+            } else {
+              return Center(
+                child: ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text('${snapshot.data![index].id}'),
+                      subtitle: Text(snapshot.data![index].title),
+                    );
+                  },
+                ),
+              );
+            }
+        }
+      },
     );
   }
 }
